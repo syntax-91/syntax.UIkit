6813bee60ee569231d4885fd83d59a64396c8dc3
syntax <dlscam112@gmail.com>
2025-05-25 01:50:01 +0500

    Update modal.tsx

--- modal.tsx
+++ modal.tsx
@@ -15,67 +15,66 @@ export function Modal({
 	...rest
 }: ModalProps) {
 	//
+
 	const handleClick = () => {
-		rest.setIsOpen(false)
+	rest.setIsOpen(false)
 	}
 
 	const refModal = TB_anim()
 	const refModalProgress = useRef<HTMLDivElement>(null)
 
 	useEffect(() => {
-		if (refModalProgress.current) {
-			refModalProgress.current.style.setProperty(
-				'--modalProgressDuration',
-				`${autoCloseDuration}ms`
-			)
-		}
+	    if (refModalProgress.current) {
+		
+		refModalProgress.current.style.setProperty(
+		'--modalProgressDuration',
+		`${autoCloseDuration}ms`
+		)
+		    
+	    }
 
 		let t: number
 
-		if (autoClose) {
-			t = setTimeout(() => rest.setIsOpen(false), autoCloseDuration)
-		}
+	if (autoClose) {
+	    t = setTimeout(() => rest.setIsOpen(false), autoCloseDuration)
+	}
 
-		return () => clearTimeout(t)
+	return () => clearTimeout(t)
 	}, [])
 
 	return (
-		<div
-			className={`//
-			${s[theme + '_c']}
-			${s['modal-container']}  
-		`}
-		>
-			<div //
-				ref={refModal} //
-				className={`
-				${s[theme]} ${s['modal']}
-				${autoClose && s['progress_']}
-				`}
-			>
-				<div
-					ref={refModalProgress} //modalProgress
-					className={s.progressBar}
-				/>
+	<div
+	    className={`//
+	    ${s[theme + '_c']}
+	    ${s['modal-container']}
+  	    `}
+	>
 
-				<span>{title}</span>
+	<div //
+	    ref={refModal} //
+	    className={`
+	    ${s[theme]} ${s['modal']}
+	    ${autoClose && s['progress_']}
+	    `}
+	>
+	<div
+	ref={refModalProgress} //modalProgress
+	className={s.progressBar}
+	/>
 
-				<p
-					className='text-center py-2 
-					'
-				>
-					{msg}
-				</p>
+	<span>{title}</span>
 
-				<Button
-					theme={theme}
-					onClick={handleClick}
-					label={titleBtn}
-					location='center'
-					max_w={100}
-				/>
-			</div>
-			{/**/}
-		</div>
+	<p className='text-center py-2'>{msg}</p>
+
+	Button
+	    theme={theme}
+	    onClick={handleClick}
+	    label={titleBtn}
+	    location='center'
+	    max_w={100}
+	/>
+	    </div>
+			
+	</div>
 	)
 }
